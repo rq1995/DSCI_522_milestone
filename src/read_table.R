7cc93ac1aa223bf4a76eb1a0bcd21cc7bf9c7f3c
RUOQI XU <7Ymr@AppledeAir-3.hitronhub.home>
2017-12-03 02:26:04 -0800

    read table
    Create a  R script to read table

---
+++ src/read_table.R
@@ -0,0 +1,5 @@
+#read the titanic table from github
+titanic=read.csv("https://raw.github.ubc.ca/ubc-mds-2017/datasets/master/data/titanic.csv?token=AAADqJP51vdVguMXFQPxX5NdnXZI8q8Kks5aLQr-wA%3D%3D", header=TRUE)
+#show the first 6 line from titanic table
+head(titanic)
+str(titanic)
