454bad075d87cb619a7f8acacada072b59477baa
RUOQI XU <7Ymr@AppledeAir-3.hitronhub.home>
2017-12-10 10:03:57 -0800

    Create clean data

--- src/clean_data.R
+++ src/clean_data.R
@@ -6,12 +6,14 @@
 # It will delete the NA for specific columns.
 # This script takes a filename and column names as the variable argument.
 #
-# Usage: Rscript clean_data.R titanic.csv survived
+# Usage: Rscript clean_data.R titanic.csv survived age fare
 
 # read in command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- args[1]
-variable <- args[2]
+variable_1 <- args[2]
+variable_2 <- args[3]
+variable_3 <- args[3]
 library(tidyverse)
 
 # define main function
@@ -21,11 +23,11 @@ main <- function(){
   data <- read.csv(input_file,header=TRUE)
   
   # clean data
-  data <- data%>%
-    filter(is.na(variable)==FALSE)
+  data1 <- data%>%
+    filter(is.na(variable_1)==FALSE,is.na(variable_2)==FALSE,is.na(variable_3)==FALSE)
   
   # write clean data to new csv file
-  write.csv(data,file="/Users/apple/Documents/777/UBC/block3/DSCI_522/DSCI_522_milestone/data/titanic_clean.csv")
+  write.csv(data1,file="/Users/apple/Documents/777/UBC/block3/DSCI_522/DSCI_522_milestone/data/titanic_clean.csv")
 }
 
 
