df4bbf7c2006165ceff00facc0df191a7076f472
RUOQI XU <7Ymr@AppledeAir-3.hitronhub.home>
2017-12-10 08:55:52 -0800

    update read_data Rscript

--- src/read_table.R
+++ src/read_table.R
@@ -1,5 +1,20 @@
-#read the titanic table from github
-titanic=read.csv("https://raw.github.ubc.ca/ubc-mds-2017/datasets/master/data/titanic.csv?token=AAADqJP51vdVguMXFQPxX5NdnXZI8q8Kks5aLQr-wA%3D%3D", header=TRUE)
+#! /usr/bin/env Rscript
+#read_table.R
+#Stephanie Xu, Dec 2017
+#
+#This script read the titanic table from github, 
+#and then write a csv file to this repo.
+#
+#Usage: Rscript read_table.R
+#Output: data file
+
+#read data from github link
+titanic=read.csv("https://raw.github.ubc.ca/ubc-mds-2017/datasets/master/data/titanic.csv?token=AAADqC0ANzUKk2ZCPXF4Sb13ucDojAHxks5aNpHbwA%3D%3D", header=TRUE)
+
 #show the first 6 line from titanic table
 head(titanic)
 str(titanic)
+
+#write csv file to data folder
+write.csv(titanic,file="/Users/apple/Documents/777/UBC/block3/DSCI_522/DSCI_522_milestone/data/titanic")
+
